3677279316b4a04ccead01d9a3169078c1b261f5
Michael Hope Setriakor <hopemichael720@gmail.com>
2021-08-17 03:44:38 +0000

    refactor(): added payment methods view

--- payments/models.py
+++ payments/models.py
@@ -1,18 +1,18 @@
 from django.db import models
-# from customers.models import Customer
-# from invoices.models import Invoice
+from customers.models import Customer
+from invoices.models import Invoices
  
 # Create your models here.
-# class Taxes(models.Model):
-#     name = models.CharField(max_length=191)
-#     percentage = models.DecimalField(decimal_places=2, max_digits=10)
-#     description = models.CharField(max_length=191, blank=True, null=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now_add=True)
-#     deleted_at = models.DateTimeField(blank=True, null=True)
+class Taxes(models.Model):
+    name = models.CharField(max_length=191)
+    percentage = models.DecimalField(decimal_places=2, max_digits=10)
+    description = models.CharField(max_length=191, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
 
-#     def __str__(self):
-#         return "{} | {}".format(self.name, self.percentage)
+    def __str__(self):
+        return "{} | {}".format(self.name, self.percentage)
 
 
 # # Create your models here.
@@ -27,16 +27,24 @@ class PaymentMethods(models.Model):
         return "{}".format(self.name)
 
 
-# class Payments(models.Model):
-#     payment_no = models.CharField(max_length=191)  # PAY00001 + 1
-#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-#     invoice = models.PositiveIntegerField()
-#     payment_method = models.ForeignKey(Invoice, on_delete=models.CASCADE)
-#     payment_date = models.DateField()
-#     cheque_no = models.CharField(max_length=191, blank=True, null=True)
-#     amount_due = models.DecimalField(decimal_places=2, max_digits=10)
-#     amount_paid = models.DecimalField(decimal_places=2, max_digits=10)
-#     balance_due = models.DecimalField(decimal_places=2, max_digits=10)
+class Payments(models.Model):
+    payment_no = models.CharField(max_length=191)  # PAY00001 + 1
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer_first_name = models.CharField(max_length=255)
+    customer_middle_name = models.CharField(max_length=255, blank=True, null=True)
+    customer_email = models.EmailField(blank=True, null=True)
+    customer_phone = models.CharField(max_length=20)
+    customer_last_name = models.CharField(max_length=255)
+    invoice_no = models.CharField(max_length=255)
+    payment_method = models.CharField(max_length=255)
+    payment_date = models.DateTimeField()
+    amount_due = models.DecimalField(decimal_places=2, max_digits=10)
+    amount_paid = models.DecimalField(decimal_places=2, max_digits=10)
+    balance_due = models.DecimalField(decimal_places=2, max_digits=10)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
 
 
 
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,175 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+import datetime
+# import pandas as pd
 
+from rest_framework import generics, permissions
+from .models import Invoices, Payments
+from quotations.models import Quotations
+
+from .serializers import PaymentSerializer
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import random
+
+from rest_framework.decorators import action
+import logging
+from pathlib import Path
 # Create your views here.
+
+ROOT_DIR = Path('__file__').resolve().parent
+
+logging.basicConfig(filename=str(ROOT_DIR)+'/logs/amata.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    level=logging.DEBUG)
+
+
+class PaymentMethodsViewSet(viewsets.ViewSet):
+
+    def list(self, request): 
+        try:
+            payment_method = PaymentMethod.objects.filter(deleted_at=None)
+            serializer = PaymentSerializer(payment_method, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request): 
+        try:
+            payment_method = request.data
+            payment_serializer = PaymentSerializer(data=payment_method)
+            payment_serializer.is_valid(raise_exception=True)
+            payment_serializer.save()
+
+            return Response({"payment_method": payment_method}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            logging.error(str(e))
+            return Response({"response":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None): 
+        try:
+            payment_method = PaymentMethod.objects.get(id=pk, deleted_at=None)
+            payment_serializer = PaymentSerializer(payment_method)
+
+            return Response(payment_serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            logging.error(str(e))
+            return Response({"response":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def update(self, request, pk=None): 
+        try:
+            # payment_method
+            payment_method = PaymentMethod.objects.get(id=pk)
+
+            payment_serializer = PaymentSerializer(instance=payment_method, data=request.data)
+            payment_serializer.is_valid(raise_exception=True)
+            payment_serializer.save()
+
+            return Response(request.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            logging.error(str(e))
+            return Response({"response":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def destroy(self, request, pk=None):
+        try:
+            payment_method = PaymentMethod.objects.get(id=pk)
+            serializer = InvoicesSerializer(instance=payment_method)
+            request_instance = dict(serializer.data)
+            request_instance['deleted_at'] = datetime.datetime.now()
+            payment_serializer = PaymentSerializer(instance=payment_method, data=request_instance)
+            payment_serializer.is_valid(raise_exception=True)
+            payment_serializer.deleted_at=datetime.datetime.now()
+            payment_serializer.save()
+
+            return Response({"response":"Invoice deleted successfully"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            logging.error(str(e))
+            return Response({"response":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PaymentsViewSet(viewsets.ViewSet):
+
+    def list(self, request): 
+        try:
+            payment = Payments.objects.filter(deleted_at=None)
+            serializer = PaymentSerializer(payment, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request): 
+        try:
+            payment = request.data
+
+            # payment serializer
+            random_pay_code = random.randint(10000,900000)
+            payment["payment_no"] = "PAY"+str(random_pay_code)
+
+            if not Invoices.objects.filter(invoice_no=str(payment['invoice_no'])).exists():
+                return Response({"response":"Invalid Invoice Number"}, status=status.HTTP_400_BAD_REQUEST)
+             
+            payment_serializer = PaymentSerializer(data=payment)
+            payment_serializer.is_valid(raise_exception=True)
+            payment_serializer.save()
+
+            return Response({"payment": payment}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            logging.error(str(e))
+            return Response({"response":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None): 
+        try:
+            payment = Payments.objects.get(id=pk, deleted_at=None)
+            payment_serializer = PaymentSerializer(payment)
+
+            return Response(payment_serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            logging.error(str(e))
+            return Response({"response":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def update(self, request, pk=None): 
+        try:
+            # payment
+            payment = Payments.objects.get(id=pk)
+
+            payment_serializer = PaymentSerializer(instance=payment, data=request.data)
+            payment_serializer.is_valid(raise_exception=True)
+            payment_serializer.save()
+
+            return Response(request.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            logging.error(str(e))
+            return Response({"response":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def destroy(self, request, pk=None):
+        try:
+            payment = Payments.objects.get(id=pk)
+            serializer = InvoicesSerializer(instance=payment)
+            request_instance = dict(serializer.data)
+            request_instance['deleted_at'] = datetime.datetime.now()
+            payment_serializer = PaymentSerializer(instance=payment, data=request_instance)
+            payment_serializer.is_valid(raise_exception=True)
+            payment_serializer.deleted_at=datetime.datetime.now()
+            payment_serializer.save()
+
+            return Response({"response":"Invoice deleted successfully"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            logging.error(str(e))
+            return Response({"response":str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    # @csrf_exempt
+    @action(detail=False, methods=['get'])
+    def fetchByNo(self, request, pk=None):
+        try:
+            payment = Payments.objects.get(payment_no=pk, deleted_at=None)
+            serializer = PaymentSerializer(payment)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"response": str(e)}, status=status.HTTP_400_BAD_REQUEST)
