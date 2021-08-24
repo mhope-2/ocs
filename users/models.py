0d69c7a657cade15c0d82995d3adfd697883430b
Michael Hope Setriakor <hopemichael720@gmail.com>
2021-08-24 02:41:07 +0000

    refactor(): updated roles model

--- users/models.py
+++ users/models.py
@@ -5,6 +5,9 @@ import uuid
 
 class Role(models.Model):
     role = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return "{}".format(self.role) 
