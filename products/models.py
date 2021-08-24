ce4ac4129fb05b7c722001b4c567db964f7b9155
Michael Hope Setriakor <hopemichael720@gmail.com>
2021-08-24 02:58:51 +0000

    refactor(): updated roles model

--- products/models.py
+++ products/models.py
@@ -6,7 +6,6 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    brand = models.CharField(max_length=255)
     description = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
